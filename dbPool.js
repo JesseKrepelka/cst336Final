c583b27a16534cf4fcc389acb45700b8fa85f2b8
= <jeff2invest@gmail.com>
2020-07-26 15:35:04 -0700

    adding db creds

--- dbPool.js
+++ dbPool.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool  = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.dbHost || "ofcmikjy9x4lroa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: process.env.dbUser || "grc5vg5b95vt0otr" ,
-    password: process.env.dbPassword || "x0txo9avldwzn578" ,
-    database: process.env.database  || "a65eyuwjnkfu86or"
+    host: process.env.dbHost || "s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: process.env.dbUser || "fxwtqjetkgq9pvek" ,
+    password: process.env.dbPassword || "knkxbmu2lb28269b" ,
+    database: process.env.database  || "uq150fxkd69erms1"
 });
 
 module.exports = pool;
