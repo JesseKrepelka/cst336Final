fe8ff90e42cb554b5c8751e950b76296b32d5a4e
EC2 Default User <ec2-user@ip-172-31-25-183.us-west-1.compute.internal>
2020-08-11 17:42:00 +0000

    shopping cart WIP

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
     $("#btnUpdateBook").click(function () {
         updateBook();
     });
@@ -19,10 +18,60 @@ $(document).ready(function () {
                 stock: $("#stock").val()
             },
             success: function (result, status) {
-                console.log(result.status)
+                console.log(result.status);
             }
         });
         
     }
-
+    
+    function loadCart(){
+        var passCart = JSON.parse(localStorage.getItem("cart"));
+        $.ajax({
+           method: "GET",
+           url: "/cartContents",
+           data: {
+               cart: passCart
+           },
+           success: function (result, status){
+               $("#cart").html(result);
+           }
+        });
+    }
+    loadCart();
+    
+    $(".btnAddToCart").click(function(e){
+        console.log(e.target.id);
+        addToCart(e.target.id);
+        console.log(localStorage.cartSize);
+    });
+    
+    function addToCart(idToAdd){
+        var tempCart = [];
+        if(typeof(Storage) !== "undefined"){
+            if(localStorage.cartSize){
+                if(localStorage.cart){
+                    tempCart = JSON.parse(localStorage.getItem("cart"));
+                    tempCart[localStorage.cartSize] = idToAdd;
+                    localStorage.cartSize = Number(localStorage.cartSize)+1;
+                    localStorage.setItem("cart", JSON.stringify(tempCart));
+                    console.log("item added");
+                }
+            } else {
+                tempCart[0] = idToAdd;
+                localStorage.setItem("cartSize", 1);
+                localStorage.setItem("cart", JSON.stringify(tempCart));
+                console.log("cart created");
+            }
+        }
+    }
+    
+    /*
+    $("#btnRemoveFromCart").click (function(){
+        removeFromCart();
+    });
+    
+    function removeFromCart(){
+        
+    }
+    */
 });
