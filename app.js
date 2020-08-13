80f6d84729a6e0c1ad113f935b0ebcdd9cdab181
EC2 Default User <ec2-user@ip-172-31-25-183.us-west-1.compute.internal>
2020-08-13 17:51:35 +0000

    Making heroku happy

--- app.js
+++ app.js
@@ -286,16 +286,17 @@ app.post("/bookDelete", function (req, res) {
 
 //Starting the web server
 //NOte can't put in other info or heroku won't work
+/*
  app.listen(port, ip,
     function () {
         console.log("Express server is running");
     }); 
-/*   
+*/   
 app.listen(process.env.PORT, process.env.IP,
     function () {
         console.log("Express server is running");
 });
-*/
+
 /*
 app.listen(3000,function(){
     console.log("Express server is running");
